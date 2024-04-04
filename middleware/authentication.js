03e4ff2a2914266c6d9d05834de9bae2741c7f2a
riya2509 <91462077+riya2509@users.noreply.github.com>
2024-04-04 19:29:42 +0530

    Middleware

---
+++ middleware/authentication.js
@@ -0,0 +1,21 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+dotenv.config();
+
+export const isAuthenticated = (req, res, next) => {
+  const token = req.headers.authorization?.replace("Bearer ", "");
+  try {
+    const decoded = jwt.verify(token, process.env.SECRET);
+    req.id = decoded.id;
+
+    next();
+  } catch (e) {
+    console.log(e);
+    console.log({
+      message: `Unauthenticated user`,
+      reason: "Issue with jwt",
+      status: 401,
+    });
+    res.status(401).send({ message: `Please login again` });
+  }
+};
