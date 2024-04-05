f272dd1dc4586f17c3b585708bed10cac718dced
riya2509 <91462077+riya2509@users.noreply.github.com>
2024-04-05 21:14:10 +0530

    refactoring

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import cors from "cors";
 import authRoutes from "./routes/authRoutes.js";
-// import appRoutes from "./app/routes/appRoutes.js";
+import appRoutes from "./routes/appRoutes.js";
 
 import env from "dotenv";
 env.config();
@@ -12,7 +12,7 @@ const { PORT } = process.env;
 const app = express();
 app.use(express.json());
 app.use("/auth", authRoutes);
-// app.use("/api", appRoutes);
+app.use("/api", appRoutes);
 
 app.listen(PORT, () =>
   console.log(`App started on PORT ${PORT} as ${new Date().toUTCString()}`)
